a9e6722128aa4b59411abc1ec2865f66d3a3220e
Hinda216 <hindaho216@gmail.com>
2025-04-20 20:37:52 +1000

    feat: initialise backend and drizzle ORM

---
+++ src/routers/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 8080;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/v1/popularProducts", (req, res) => {
+    res.json({
+        products: [
+            {
+                id: 1,
+                name: "埃塞俄比亚耶加雪菲",
+                image: "https://images.unsplash.com/photo-1559056199-641a0ac8b55e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1440&q=80",
+                description: "花香果酸，清爽回甘",
+                price: 128
+            },
+        ],
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
