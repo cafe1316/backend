a961606f7c45a0b6c4594d27f7b2af48d51d2cc4
Hinda216 <hindaho216@gmail.com>
2025-07-08 22:39:05 +1000

    Initial commit for feature/api

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,7 @@
 import db from "../db";
 import { cafe1316Users } from "../../drizzle/schema";
 import { eq } from "drizzle-orm";
+import { hash } from "bun";
 
 interface RegisterForm {
   username: string;
@@ -36,5 +37,11 @@ export const registerNewUser = async (params: RegisterForm) => {
     username, password, email
   })
   // TODO: password needs hashing
+  const bcrypt = require('bcrypt');
+  const hashPassword = 'password';
+  const saltRounds = 10;
+  bcrypt.hash(hashPassword, saltRounds, (err,hash)) => {
+    if(err)
+  }
   // TODO: jwt token
 };
